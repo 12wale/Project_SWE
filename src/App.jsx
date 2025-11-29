a62259ff8325ce0a207a0ddd535d6a7bfe0beb71
12wale <wlydrftm399@gmail.com>
2025-11-29 20:26:50 +0200

    edit master

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,33 @@ import Header from "./layouts/Header";
 import Footer from "./layouts/Footer";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import Contact from './Pages/Contact'
+import { useEffect } from 'react'
+import ScrollToTop from './layouts/ScrollToTop'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 import AuthPage from "./components/LoginSign/AuthPage";
 import Dashboard from "./components/LoginSign/Dashboard";
 import ProtectedRoute from "./components/LoginSign/ProtectedRoute";
 
 export default function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    })
+  }, [])
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+
         <Route path="/auth" element={<AuthPage />} />
         <Route
           path="/dashboard"
@@ -30,3 +45,41 @@ export default function App() {
     </BrowserRouter>
   );
 }
+
+/**
+ * import { BrowserRouter, Route, Routes } from 'react-router'
+import Header from './layouts/Header'
+import Footer from './layouts/Footer'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import { useEffect } from 'react'
+import ScrollToTop from './layouts/ScrollToTop'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    })
+  }, [])
+
+  return (
+    <>
+     <BrowserRouter>
+     <Header />
+     <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
+ */
